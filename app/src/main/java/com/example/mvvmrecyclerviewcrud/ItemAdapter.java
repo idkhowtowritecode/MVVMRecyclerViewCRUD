a36d68b80869cc98a599b9fcfb522dbb9c95a331
Well Zheng <cheng8703144@outlook.com>
2023-07-29 01:13:35 +0800

    a simple recycler view chatGPT taught me and enable the data binding features in the project

---
+++ app/src/main/java/com/example/mvvmrecyclerviewcrud/ItemAdapter.java
@@ -0,0 +1,38 @@
+package com.example.mvvmrecyclerviewcrud;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+import androidx.recyclerview.widget.RecyclerView;
+public class ItemAdapter extends RecyclerView.Adapter<ItemAdapter.ViewHolder> {
+    private String[] data;
+
+    public ItemAdapter(String[] data) {
+        this.data = data;
+    }
+
+    @Override
+    public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_list, parent, false);
+        return new ViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(ViewHolder holder, int position) {
+        holder.textView.setText(data[position]);
+    }
+
+    @Override
+    public int getItemCount() {
+        return data.length;
+    }
+
+    public static class ViewHolder extends RecyclerView.ViewHolder {
+        public TextView textView;
+
+        public ViewHolder(View itemView) {
+            super(itemView);
+            textView = itemView.findViewById(R.id.textView);
+        }
+    }
+}
--- app/src/main/java/com/example/mvvmrecyclerviewcrud/MainActivity.java
+++ app/src/main/java/com/example/mvvmrecyclerviewcrud/MainActivity.java
@@ -1,15 +1,26 @@
 package com.example.mvvmrecyclerviewcrud;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
-
+    private RecyclerView recyclerView;
+    private ItemAdapter adapter;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        System.out.println("hello world");
+
+        recyclerView = findViewById(R.id.recyclerView);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+
+        String[] data = {"Item 1", "Item 2", "Item 3", "Item 4", "Item 5"};
+
+
+        adapter = new ItemAdapter(data);
+        recyclerView.setAdapter(adapter);
     }
 }
