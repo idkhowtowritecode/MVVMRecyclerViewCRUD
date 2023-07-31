2e36922da782cefa615d49980f507343af6f0799
Well Zheng <cheng8703144@outlook.com>
2023-08-01 00:53:53 +0800

    save and cancel button in the dialog

--- app/src/main/java/com/example/mvvmrecyclerviewcrud/CustomDialog.java
+++ app/src/main/java/com/example/mvvmrecyclerviewcrud/CustomDialog.java
@@ -2,6 +2,9 @@ package com.example.mvvmrecyclerviewcrud;
 import android.app.Dialog;
 import android.content.Context;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class CustomDialog extends Dialog{
     private Context context;
@@ -17,6 +20,21 @@ public class CustomDialog extends Dialog{
 
         // You can find and handle the UI components here
         // For example, set click listeners or get values from the UI components
+        Button saveButton = findViewById(R.id.buttonSave);
+        Button cancelButton = findViewById(R.id.buttonCancel);
+
+        saveButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(view.getContext(), "Save", Toast.LENGTH_SHORT).show();
+            }
+        });
+        cancelButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                dismiss();
+            }
+        });
 
     }
 }
